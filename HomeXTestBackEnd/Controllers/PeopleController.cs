f2a6c4abda40d2491f678905f4f25f28e2a821fa
Harold Rodriguez <haroldkt@gmail.com>
2018-08-11 08:06:36 -0400

    Added console app and WebAPI OWIN packages

---
+++ HomeXTestBackEnd/Controllers/PeopleController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace HomeXTestBackEnd.Controllers
+{
+    public class PeopleController: ApiController
+    {
+        // GET api/values 
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5 
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
--- HomeXTestBackEnd/Program.cs
+++ HomeXTestBackEnd/Program.cs
@@ -1,15 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
 
 namespace HomeXTestBackEnd
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var baseAddress = "http://localhost:9000/";
+
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                // Create HttpCient and make a request to api/values 
+                var client = new HttpClient();
+                var response = client.GetAsync(baseAddress + "api/people").Result;
+
+                Console.WriteLine(response);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                Console.ReadLine();
+            }
         }
     }
 }
